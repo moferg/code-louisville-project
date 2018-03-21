12456ae72ff2f7b121c2e9d66e45324d3074183f
Marshall <moferg01@louisville.edu>
2018-03-21 18:32:17 -0400

    Made function move heading in response to hamburger menu opening.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 function toggleNav() {
+
     var nav = document.getElementById("hidden");
+    var heading = document.getElementById("content-heading");
+    // Function makes heading move in response to hamburger menu opening
+    
     if (nav.className === "hidden") {
         nav.className = " shown";
+        heading.style.marginTop = "250px";
     }
     else {
         nav.className = "hidden";
+        heading.style.marginTop = "initial";
     }
 }
 // TODO: Rewrite using jQuery
-// TODO: Change header top-margin after clicking hamburger icon
